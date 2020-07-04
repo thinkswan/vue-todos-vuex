ceb37496b363758df4d683cab18e3970f59c685b
Graham Swan <thinkswan@gmail.com>
2020-07-04 21:34:08 +0200

    feat: add <Todos> component

---
+++ src/components/Todos.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>Todos</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todos"
+};
+</script>
+
+<style>
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  line-height: 1.6;
+  background-color: #e8f7f0;
+}
+
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
+</style>
